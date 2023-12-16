e20ba213d40c74c56586a34f56ff5c6447dfe6bc
Vuvi <150832283+VuviStudio@users.noreply.github.com>
2023-12-16 22:55:04 +0100

    Update VuviAdvertiser.bat

--- VuviAdvertiser.bat
+++ VuviAdvertiser.bat
@@ -10,10 +10,30 @@ if %input%==%KEY% (
     echo.
     echo Key Accepted. Proceeding with the advertiser...
     timeout /t 3 /nobreak >nul
-    node Advertise.js
+    call :CheckModules
 ) else (
     echo.
     echo Invalid Key. Please try again.
     timeout /t 3 /nobreak >nul
 )
 
+
+
+:CheckModules
+if not exist "node_modules" (
+    echo Node modules folder not found. Installing discord.js-selfbot-v13@latest...
+    call :InstallModules
+) else (
+    echo Node modules folder found. Continuing with Advertise.js...
+    node Advertise.js
+)
+
+
+:InstallModules
+npm install discord.js-selfbot-v13@latest
+if errorlevel 1 (
+    echo Installation failed. Please check npm and try again.
+) else (
+    echo Installation successful. Running Advertise.js...
+    node Advertise.js
+)
